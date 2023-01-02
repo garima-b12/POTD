5d407935bb71a38dc83c86688742167401da0110
garima-b12 <garimabothra2000@gmail.com>
2023-01-02 12:28:59 +0530

    Jan 2

--- jan2.cpp
+++ jan2.cpp
@@ -1,12 +1,18 @@
+/*Given a string, remove the vowels from the string */
+
 string removeVowels(string S) 
-	{
-	    for(int i=0;i<S.length();i++)
-	    {
-	        if(S[i]=='a' || S[i]=='e' || S[i]=='i' || S[i]=='o' || S[i]=='u')
-	       {
-	               S.erase(i,1);
-	               i--;
-	       }
+{
+    for(int i=0;i<S.length();i++)
+    {
+        if(S[i]=='a' || S[i]=='e' || S[i]=='i' || S[i]=='o' || S[i]=='u')
+        {
+	        S.erase(i,1);
+	        i--;
 	    }
-	    return S;
 	}
+	return S;
+}
+
+/* Time complexity: O(n)
+   Space Complexity: O(1)
+*/
