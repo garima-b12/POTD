c6e68ab4de0327a5ed18fb02d55b3cee8dca387c
garima-b12 <garimabothra2000@gmail.com>
2023-01-08 01:07:30 +0530

    Jan 8

--- jan8_task1.cpp
+++ jan8_task1.cpp
@@ -2,3 +2,10 @@
 	Push all the zeros of the given array to the right end of the array while maitaining the order of non-zero elements.
 	gfg link: https://practice.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1
 */
+
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    // code here
+	}
+};
