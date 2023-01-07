d9f6f036d3162657941e0fe2226be877e40616ac
garima-b12 <garimabothra2000@gmail.com>
2023-01-08 01:05:45 +0530

    Jan 8

---
+++ jan8_task1.cpp
@@ -0,0 +1,4 @@
+/* 	Given an array arr[] of N positive integers. 
+	Push all the zeros of the given array to the right end of the array while maitaining the order of non-zero elements.
+	gfg link: https://practice.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1
+*/
