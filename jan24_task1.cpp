b8dd6bda300182c214f63b22169691b1b1520dc7
garima-b12 <garimabothra2000@gmail.com>
2023-01-24 02:34:31 +0530

    Two Sum

---
+++ jan24_task1.cpp
@@ -0,0 +1,26 @@
+/*	Two Sum
+	leetcode link: https://leetcode.com/problems/two-sum/
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i]) != m.end())
+            {
+                ans.push_back(i);
+                ans.push_back(m[target-nums[i]]);
+            }
+            else
+                m[nums[i]]=i;
+        }
+        return ans;
+    }
+};
+
+/*	TC: O(n)
+	SC: O(n)
+*/
